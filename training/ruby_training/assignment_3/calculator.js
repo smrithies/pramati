e8063679cf2ed56212fa4ed2fb2f3f08cd2d281c
smrithies <smrithies@gmail.com>
2018-10-10 18:31:48 +0530

    added some functions

--- training/ruby_training/assignment_3/calculator.js
+++ training/ruby_training/assignment_3/calculator.js
@@ -31,4 +31,29 @@ $(document).ready(function () {
   $("#percent").click(function () {
     $("#display1").val(eval(($("#display1").val())/100));
   });
+  $("#factorial-func").click(function () {
+    $("#display1").val(numFactorial($("#display1").val()));
+  });
+  $("#absolute-func").click(function () {
+    $("#display1").val(Math.abs($("#display1").val()));
+  });
+  $("#sqrt-func").click(function () {
+    $("#display1").val(Math.sqrt($("#display1").val()));
+  });
+  $("#pi").click(function () {
+    $("#display1").val(Math.PI);
+  });
+
+
+  function numFactorial(number){
+    let output = 1;
+    if(number <= 0){
+             output = 1;
+    }else{
+      for(var i=1; i<=number; i++){
+        output = output*i;
+      }
+    }
+    return output;
+  }
 });
